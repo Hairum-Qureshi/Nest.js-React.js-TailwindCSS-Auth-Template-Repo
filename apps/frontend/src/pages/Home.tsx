dfff557bd5f3070faa9975511896d5a71e1d4978
Hairum Qureshi <hqureshi@udel.edu>
2026-08-18 13:45:25 -0400

    added some more explanatory text in Home page

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -5,8 +5,26 @@ export default function Home() {
   const { data: currUserData } = useCurrentUser();
 
   return (
-    <div className="min-h-screen bg-white relative overflow-hidden flex flex-col">
-      <h1 className="p-10 text-2xl font-semibold text-black">Hello World</h1>
+    <div className="min-h-screen max-h-auto p-10 bg-white relative overflow-hidden flex flex-col">
+      <h1 className="text-3xl font-semibold text-black">Hello</h1>
+      <p className="text-lg font-semibold my-5">
+        If you're not seeing the Google OAuth button, it's due to the{" "}
+        <code className="bg-gray-100 text-base text-green-600 p-1 rounded">
+          /api/auth/current-user
+        </code>{" "}
+        API endpoint returning HTML so technically{" "}
+        <code className="bg-gray-100 text-base text-green-600 p-1 rounded">
+          currUserData
+        </code>{" "}
+        isn't undefined. Once you provide the appropriate backend{" "}
+        <code className="bg-gray-100 text-base text-green-600 p-1 rounded">
+          env
+        </code>{" "}
+        variables and the appropriate data, the endpoint will work and you'll
+        see the button below.
+      </p>
+     
+
       <div className="flex flex-col items-center gap-4">
         {!currUserData && <GoogleOAuthButton />}
       </div>
