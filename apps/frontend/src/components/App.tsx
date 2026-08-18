fec715c94b5fb011a83cfcdf0a5bba7a3f497ddd
Hairum Qureshi <hqureshi@udel.edu>
2026-08-18 13:27:28 -0400

    wrapped routes in GoogleOAuthProvider

--- apps/frontend/src/components/App.tsx
+++ apps/frontend/src/components/App.tsx
@@ -3,15 +3,20 @@ import Home from "../pages/Home";
 import About from "../pages/About";
 import NotFound from "../pages/NotFound";
 import "../css/index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <GoogleOAuthProvider
+        clientId={import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </GoogleOAuthProvider>
+    </BrowserRouter>
+  );
 }
