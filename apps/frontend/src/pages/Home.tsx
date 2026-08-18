effcb2062ede8e2af6f7a07660b6b7999b175d5a
Hairum Qureshi <hqureshi@udel.edu>
2026-08-18 13:29:10 -0400

    added styling to Hello World button and changed background color

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   const { data: currUserData } = useCurrentUser();
 
   return (
-    <div className="min-h-screen bg-slate-950 text-white relative overflow-hidden flex flex-col">
-      <h1>Hello World</h1>
+    <div className="min-h-screen bg-white relative overflow-hidden flex flex-col">
+      <h1 className="p-10 text-2xl font-semibold text-black">Hello World</h1>
       <div className="flex flex-col items-center gap-4">
         {!currUserData && <GoogleOAuthButton />}
       </div>
