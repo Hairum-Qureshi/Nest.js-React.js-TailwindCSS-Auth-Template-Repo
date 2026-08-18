8030d69d2411d0391e71753fc51fc5ea7399e3d6
Hairum Qureshi <hqureshi@udel.edu>
2026-08-18 13:25:50 -0400

    added Hello World text

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -2,15 +2,14 @@ import GoogleOAuthButton from "../components/GoogleOAuthButton";
 import { useCurrentUser } from "../hooks/useCurrentUser";
 
 export default function Home() {
-	const { data: currUserData } = useCurrentUser();
+  const { data: currUserData } = useCurrentUser();
 
-	return (
-		<div className="min-h-screen bg-slate-950 text-white relative overflow-hidden flex flex-col">
-				<div className="flex flex-col items-center gap-4">
-					{!currUserData && (
-						<GoogleOAuthButton />
-					)}
-				</div>
-		</div>
-	);
+  return (
+    <div className="min-h-screen bg-slate-950 text-white relative overflow-hidden flex flex-col">
+      <h1>Hello World</h1>
+      <div className="flex flex-col items-center gap-4">
+        {!currUserData && <GoogleOAuthButton />}
+      </div>
+    </div>
+  );
 }
